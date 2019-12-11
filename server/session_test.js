6a471624f766a3c92c87787e6e0a609aa163eba2
SpunkyAardvark123 <ktsanter@gmail.com>
2019-12-11 14:45:38 -0500

    first pass at a login page sequence

--- server/session_test.js
+++ server/session_test.js
@@ -1,6 +1,6 @@
 var express = require('express')
-//var parseurl = require('parseurl')
 const path = require('path')
+var bodyParser = require('body-parser');
 var session = require('express-session')
 var mySQL = require('mysql')
 var MySQLStore = require('express-mysql-session')(session);
@@ -8,65 +8,71 @@ var MySQLStore = require('express-mysql-session')(session);
 var app = express()
 const port = 3000;
 
-var bodyParser = require('body-parser');
-app.use(bodyParser.json()); 
+//------ req body parsers -------------
+  app.use(bodyParser.json()); 
+  app.use(bodyParser.urlencoded({ extended: true })); // for form data
 
-var mysqlPool = mySQL.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'SwordFish002',
-    database: 'session_test'
-});
+//------- session management ------------------
+  var mysqlPool = mySQL.createPool({
+      host: 'localhost',
+      user: 'root',
+      password: 'SwordFish002',
+      database: 'session_test'
+  });
 
-var sessionStore = new MySQLStore({
-    clearExpired: true,
-    checkExpirationInterval: 900000,
-    createDatabaseTable: true  
-  }, 
-  mysqlPool
-);
+  var sessionStore = new MySQLStore({
+      clearExpired: true,
+      checkExpirationInterval: 900000,
+      createDatabaseTable: true  
+    }, 
+    mysqlPool
+  );
 
-app.use(session({
-  secret: 'Grit Gumption',
-  cookie: {maxAge: 300000}, 
-  resave: false,
-  saveUninitialized: true,
-  store: sessionStore
-}))
+  app.use(session({
+    secret: 'Grit Gumption',
+    cookie: {maxAge: 300000}, 
+    resave: false,
+    saveUninitialized: true,
+    store: sessionStore
+  }))
 
-app.use(function (req, res, next) {
-  if (!req.session.userInfo) {
-    req.session.userInfo = {userId: -1}
-  }
+  //-- initialize userInfo, set ID to "not logged-in" sentinel value
+  app.use(function (req, res, next) {
+    if (!req.session.userInfo) {
+      req.session.userInfo = {userId: -1}
+    }
 
-  next()
-})
+    next()
+  })
+// end of session management ************
 
-app.get('/mainpage.html', function (req, res) {
-  console.log('trying to access mainpage.html');
+//-------- login management ---------------------
+  app.get('/mainpage.html', function (req, res) {
+    var success = (req.session.userInfo && req.session.userInfo.userId >= 0);
 
-  var htmlFile = 'mainpage.html';
-  if (!req.session.userInfo || req.session.userInfo.userId < 0) {
-    htmlFile = 'login.html';
-  }
-  
-  res.sendFile(path.join(__dirname, 'private', htmlFile))
-})
+    if (success) {
+      res.sendFile(path.join(__dirname, 'private', 'mainpage.html'))
+    } else {
+      res.redirect('/login.html');
+    }
+  })
 
-app.post('/login', function (req, res) {
-  var userName = req.body.userName;
-  var userPassword = req.body.userPassword;
+  app.get('/login.html', function (req, res) {
+    res.sendFile(path.join(__dirname, 'private', 'login.html'))
+  })
 
-  var result = {success: false, details: 'login failed'};  
-  if (userPassword == 'okay') {
-    result.success = true;
-    result.details = 'login succeeded';
-    req.session.userInfo.userId = 1;
-  }
-
-  console.log('login attempt: ' + userName + ' ' + userPassword + ' - ' + (result.success ? 'succeeded' : 'failed'));
-
-  res.send(result);
-})
+  app.post('/login_attempt', function (req, res) {
+    var userName = req.body.userName;
+    var userPassword = req.body.userPassword;
+    var success = (userPassword == 'okay');
+   
+    if (success) {
+      req.session.userInfo.userId = 1;
+      res.redirect('/mainpage.html');
+    } else {
+      res.redirect('/login.html?retry=true');
+    }
+  })
+//----------- end of login management -----------------
 
 app.listen(port, () => console.log('app listening on port ' + port));
