3b07f057198621dab69b8414b97582486b5f22fe
SpunkyAardvark123 <ktsanter@gmail.com>
2021-02-03 19:26:58 -0500

    basic-web-design: box model draft near complete

--- server/private/basic-web-design/scripts/boxmodel.js
+++ server/private/basic-web-design/scripts/boxmodel.js
@@ -47,31 +47,82 @@ const app = function () {
   function _update() {
     var controlSettings = {
       margin: _getSettings('margin', '0', 'px'),
-      border: _getSettings('border', 'none', ''),
+      border: _getSettings('border', '0', 'px'),
       padding: _getSettings('padding', '0', 'px')
     };
-     
-    console.log(controlSettings);
     
+    _setElementStyling(page.elementDemo, 'margin', controlSettings.margin);
+    _setElementStyling(page.elementDemo, 'border-width', controlSettings.border);
+    _setElementStyling(page.elementDemo, 'padding', controlSettings.padding);
+
+    _showElementCSS(page.elementText, controlSettings);
   }
   
   function _getSettings(groupname, defaultVal, unit) {
-    var settings = {
-      topval: defaultVal,
-      rightval: defaultVal,
-      bottomval: defaultVal,
-      leftval: defaultVal
-    };
+    var settings = '';
+    for (var i = 0; i < 4; i++) {
+      settings += ' ' + defaultVal + unit;
+    }
     
     var elemCheckbox = page.body.getElementsByClassName('checkbox ' + groupname)[0];
+    page.body.getElementsByClassName('slider ' + groupname + '-top')[0].disabled = !elemCheckbox.checked;
+    page.body.getElementsByClassName('slider ' + groupname + '-right')[0].disabled = !elemCheckbox.checked;
+    page.body.getElementsByClassName('slider ' + groupname + '-bottom')[0].disabled = !elemCheckbox.checked;
+    page.body.getElementsByClassName('slider ' + groupname + '-left')[0].disabled = !elemCheckbox.checked;
+
     if (elemCheckbox.checked) {
-      settings.topval = page.body.getElementsByClassName('slider ' + groupname + '-top')[0].value + unit;
-      settings.rightval = page.body.getElementsByClassName('slider ' + groupname + '-right')[0].value + unit;
-      settings.bottomval = page.body.getElementsByClassName('slider ' + groupname + '-bottom')[0].value + unit;
-      settings.leftval = page.body.getElementsByClassName('slider ' + groupname + '-left')[0].value + unit;
+      settings = page.body.getElementsByClassName('slider ' + groupname + '-top')[0].value + unit;
+      settings += ' ' + page.body.getElementsByClassName('slider ' + groupname + '-right')[0].value + unit;
+      settings +=  ' ' + page.body.getElementsByClassName('slider ' + groupname + '-bottom')[0].value + unit;
+      settings +=  ' ' + page.body.getElementsByClassName('slider ' + groupname + '-left')[0].value + unit;
+      
+    } else if (groupname == 'border') {
+      settings = 'none';
     }
     
     return settings;
+  }
+  
+  function _showElementCSS(elem, settings) {
+    console.log(settings);
+    var marginCSS = 'margin:' + settings.margin + ';';
+    
+    var borderCSS = 'border-width: ' + settings.border + '; border-style: solid;';
+    if (settings.border.includes('none')) {
+      borderCSS = 'border-style: none;';
+    }
+    
+    var paddingCSS = 'padding:' + settings.padding + ';';
+    
+    var cssText = marginCSS + '<br>' + borderCSS + '<br>' + paddingCSS;
+    
+    elem.innerHTML = cssText;
+  }
+  
+  function _setElementStyling(elem, styleName, styleSettings) {
+    console.log(styleName + ': ' + styleSettings);
+    if (styleName == 'border-width') {
+      if (styleSettings.includes('none')) {
+        elem.style.borderStyle = 'none';
+        elem.style[styleName] = '0';
+      } else {
+        elem.style.borderStyle = 'solid';
+        elem.style[styleName] = styleSettings;
+      }
+      
+    } else {
+      elem.style[styleName] = styleSettings;
+    }
+    /*
+    if (styleName == 'margin' || styleName == 'padding') {
+      elem.style[styleName + 'Top'] = styleSettings.topval;
+      elem.style[styleName + 'Right'] = styleSettings.rightval;
+      elem.style[styleName + 'Bottom'] = styleSettings.bottomval;
+      elem.style[styleName + 'Left'] = styleSettings.leftval;
+      
+    } else if (styleName = 'border') {
+    }
+    */
   }
   
 	//---------------------------------------
