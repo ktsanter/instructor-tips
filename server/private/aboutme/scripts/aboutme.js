f888137c4df4c7fd494058bee3f60428e8392c21
SpunkyAardvark123 <ktsanter@gmail.com>
2021-02-13 11:22:29 -0500

    image-flipper bug fix for styling

--- server/private/aboutme/scripts/aboutme.js
+++ server/private/aboutme/scripts/aboutme.js
@@ -5,23 +5,30 @@
 //-------------------------------------------------------------------------
 const app = function () {
 	const page = {};	
-	const settings = {};
+	const settings = {
+    flipperURL: window.location.origin + '/image-flipper/flipper?configkey=1'
+  };
 	
 	//---------------------------------------
 	// get things going
 	//----------------------------------------
 	async function init () {
     page.body = document.getElementsByTagName('body')[0];
-		page.contents = page.body.getElementsByClassName('title')[0];
+		page.title = page.body.getElementsByClassName('title')[0];
 		page.contents = page.body.getElementsByClassName('contents')[0];
+    page.flipperframe = page.contents.getElementsByClassName('flipperframe')[0];
 
-    //_renderPage();
+    _renderPage();
 	}
 	
 	//--------------------------------------------------------------
 	// rendering
 	//--------------------------------------------------------------
-	
+	function _renderPage() {
+    console.log(settings.flipperURL);
+    page.flipperframe.src = settings.flipperURL;
+  }
+  
 	//--------------------------------------------------------------
 	// handlers
 	//--------------------------------------------------------------
