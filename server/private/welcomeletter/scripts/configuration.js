0e0d74cbeba0171738fd15c1bd7037b15cc735b7
SpunkyAardvark123 <ktsanter@gmail.com>
2021-03-08 19:53:05 -0500

    welcome-letter: refactoring 75%

--- server/private/welcomeletter/scripts/configuration.js
+++ server/private/welcomeletter/scripts/configuration.js
@@ -1,7 +1,9 @@
 //-------------------------------------------------------------------
 // welcome letter configuration
 //-------------------------------------------------------------------
-// TODO: 
+// TODO: finish preview
+// TODO: finish share
+// TODO: styling
 //-------------------------------------------------------------------
 const app = function () {
   const page = {};
@@ -13,11 +15,20 @@ const app = function () {
   const settings = {
     hideClass: 'hide-me',
     navItemClass: 'use-handler',
+    
+    currentCourse: null,
+    
     logoutURL: '/usermanagement/logout/welcomeV2',
     optionsURL: '/welcomeletter/options',
     helpURL: '/welcomeletter/help',
+    previewURL_base: '/welcomeletterV2',
     
-    previewURL_base: '/welcomeletterV2'
+    dirtyBit: {
+      navConfiguration: false,
+      navPreview: false,
+      navShare: false,
+      navProfile: false
+    }
   };
 
   //---------------------------------------
@@ -78,7 +89,7 @@ const app = function () {
     var allowOptions = page.body.getElementsByClassName('allowoptions')[0].innerHTML == 'true';
     if (!allowOptions) {
       var elemNav = document.getElementById('navEditOptions');
-      var elemDivider = document.getElementById('dividerNav1');
+      var elemDivider = document.getElementById('dividerNav2');
       UtilityKTS.setClass(elemNav, settings.hideClass, true);
       UtilityKTS.setClass(elemDivider, settings.hideClass, true);
     }
@@ -88,13 +99,51 @@ const app = function () {
   // page rendering
   //-----------------------------------------------------------------------------  
   async function _renderContents() {
+    page.elemCourseSelection = page.contentsConfiguration.getElementsByClassName('select-course')[0];
+    page.elemProjectAdd = page.contentsConfiguration.getElementsByClassName('button-add')[0];
+    page.elemProjectDelete = page.contentsConfiguration.getElementsByClassName('button-delete')[0];    
+    
+    // configuration
+    page.elemAPCourse = page.contentsConfiguration.getElementsByClassName('check-apcourse')[0];
+    page.elemHasPasswords = page.contentsConfiguration.getElementsByClassName('check-haspasswords')[0];
+    page.elemExamsSelection = page.contentsConfiguration.getElementsByClassName('select-exams')[0];
+    page.elemProctoringSelection = page.contentsConfiguration.getElementsByClassName('select-proctoring')[0];
+    page.elemRetakesSelection = page.contentsConfiguration.getElementsByClassName('select-retakes')[0];
+    page.elemResubmissionSelect = page.contentsConfiguration.getElementsByClassName('select-resubmission')[0];
+    
+    // preview
+    page.elemAudience = page.contentsPreview.getElementsByClassName('check-audience')[0];
+    page.elemAudience.leftLabel = page.elemAudience.parentNode.parentNode.getElementsByClassName('checkbox-label-left')[0];
+    page.elemAudience.rightLabel = page.elemAudience.parentNode.getElementsByClassName('checkbox-label-right')[0];
+    
+    _attachHandlers();
+    
+    await _updateCourseSelection();
+  }
+  
+  function _attachHandlers() {
+    // configuration
+    page.elemCourseSelection.addEventListener('change', (e) => { _handleCourseSelection(e); });
+    page.elemProjectAdd.addEventListener('click', () => { _addCourse(); });
+    page.elemProjectDelete.addEventListener('click', (e) => { _deleteCourse(e); });
+
+    page.elemAPCourse.addEventListener('click', (e) => { _handleConfigChange(e); });
+    page.elemHasPasswords.addEventListener('click', (e) => { _handleConfigChange(e); });
+    page.elemExamsSelection.addEventListener('change', (e) => { _handleConfigChange(e); });
+    page.elemProctoringSelection.addEventListener('change', (e) => { _handleConfigChange(e); });
+    page.elemRetakesSelection.addEventListener('change', (e) => { _handleConfigChange(e); });
+    page.elemResubmissionSelect.addEventListener('change', (e) => { _handleConfigChange(e); });
+    
+    // preview
+    page.elemAudience.addEventListener('click', (e) => { _handleAudienceChange(e); });
+    page.elemAudience.leftLabel.addEventListener('click', () => { _handleDoubleSwitch(page.elemAudience, 'left'); });
+    page.elemAudience.rightLabel.addEventListener('click', () => { _handleDoubleSwitch(page.elemAudience, 'right'); });
   }
   
   //---------------------------------------
 	// updating
 	//----------------------------------------
   async function _showContents(contentsId) {
-    console.log('_showContents: ' + contentsId);
     settings.currentNavOption = contentsId;
     
     var containers = page.contents.getElementsByClassName('contents-container');
@@ -103,9 +152,205 @@ const app = function () {
       UtilityKTS.setClass(containers[i], settings.hideClass, hide);
     }
     
+    if (contentsId == 'navPreview') await _updatePreview();
+    if (contentsId == 'navShare') _updateShare();
     if (contentsId == 'navProfile') await settings.profile.reload();
     
+    _setNavOptions();
   }
+  
+  function _setNavOptions() {
+    var opt = settings.currentNavOption;
+    var validCourse = (settings.currentCourse != null);
+    
+    _enableNavOption('navPreview', true, validCourse);
+    _enableNavOption('navShare', true, validCourse);
+    _enableNavOption('navRename', true, false);
+    _enableNavOption('dividerNav1', true, false);
+
+    _enableNavOption('navSave', false);
+    _enableNavOption('navReload', false);
+    
+    if (opt == 'navConfiguration') {
+    _enableNavOption('navRename', true, validCourse);
+    _enableNavOption('dividerNav1', true, validCourse);
+    UtilityKTS.setClass(page.elemProjectDelete, 'disabled', !validCourse);
+      
+    } else if (opt == 'navPreview') {
+      
+    } else if (opt == 'navShare') {
+
+    } else if (opt == 'navProfile') {
+      var enable = settings.profile.isDirty();
+      _enableNavOption('navSave', true, enable);
+      _enableNavOption('navReload', true, enable);
+    }
+  }
+  
+  function _enableNavOption(navOption, visible, enable) {
+    var elem = document.getElementById(navOption);
+    UtilityKTS.setClass(elem, settings.hideClass, !visible);
+    if (elem.classList.contains('btn')) {
+      elem.disabled = !enable;    
+    } else {
+      UtilityKTS.setClass(elem, 'disabled', !enable);
+    }
+  }
+    
+  function _setDirtyBit(dirty) {
+    var opt = settings.currentNavOption;
+    if (settings.dirtyBit.hasOwnProperty(opt)) {
+      settings.dirtyBit[settings.currentNavOption] = dirty;
+    }
+    _setNavOptions();
+  }  
+  
+  async function _updateCourseSelection() {
+    UtilityKTS.removeChildren(page.elemCourseSelection);
+    
+    if (!(await _queryCourseList())) return;
+
+    var indexToSelect = -1;
+    for (var i = 0; i < settings.courseInfo.length; i++) {
+      var course = settings.courseInfo[i];
+      var elem = CreateElement.createOption(null, 'course', course.courseid, course.coursename);
+      elem.courseInfo = course;
+      if (settings.currentCourse && course.courseid == settings.currentCourse.courseid) indexToSelect = i;
+      page.elemCourseSelection.appendChild(elem);
+    }
+    
+    page.elemCourseSelection.selectedIndex = indexToSelect;
+    var courseInfo = null;
+    if (indexToSelect >= 0) courseInfo = page.elemCourseSelection[indexToSelect].courseInfo;
+    _loadCourse(courseInfo);
+  }
+  
+  function _loadCourse(courseInfo) {
+    settings.currentCourse = courseInfo;
+    
+    page.elemAPCourse.disabled = !courseInfo;
+    page.elemHasPasswords.disabled = !courseInfo;
+    page.elemExamsSelection.disabled = !courseInfo;
+    page.elemProctoringSelection.disabled = !courseInfo;
+    page.elemRetakesSelection.disabled = !courseInfo;
+    page.elemResubmissionSelect.disabled = !courseInfo;
+    
+    page.elemAPCourse.checked = courseInfo && courseInfo.ap;
+    page.elemHasPasswords.checked = courseInfo && courseInfo.haspasswords;
+    page.elemExamsSelection.value = courseInfo ? courseInfo.examid: -1;
+    page.elemProctoringSelection.value = courseInfo ? courseInfo.proctoringid: -1;
+    page.elemRetakesSelection.value = courseInfo ? courseInfo.retakeid: -1;
+    page.elemResubmissionSelect.value = courseInfo ? courseInfo.resubmissionid: -1;
+    
+    _setDirtyBit(false);
+  }
+  
+  async function _updatePreview() {
+    console.log('_updatePreview');
+  }
+  
+  function _updateShare() {
+    console.log('_updateShare');
+  }
+  
+  async function _saveCourseInfo() {
+    if (!settings.currentCourse) return;
+
+    var course = settings.currentCourse;
+    
+    var configurationInfo = {
+      courseid: course.courseid,
+      coursename: course.coursename,
+      ap: page.elemAPCourse.checked,
+      haspasswords: page.elemHasPasswords.checked,
+      examid: page.elemExamsSelection.value,
+      proctoringid: page.elemProctoringSelection.value,
+      retakeid: page.elemRetakesSelection.value,
+      resubmissionid: page.elemResubmissionSelect.value,
+    };
+    
+    var result = await _queryUpdateCourse(configurationInfo);
+    if (!result.success) {
+      if (result.details.includes('duplicate')) {
+        alert('failed to rename course\n a configuration for "' + course.coursename + '" already exists');
+        page.notice.setNotice('');
+      } else {
+        page.notice.setNotice(result.details);
+      }
+    }    
+    
+    await _updateCourseSelection();
+  }
+  
+  async function _renameCourse() {
+    if (!settings.currentCourse) return;
+
+    var courseNameOrig = settings.currentCourse.coursename;
+
+    var msg = 'Please enter the new name for the course.';
+    var newCourseName = prompt(msg, courseNameOrig);
+    if (!newCourseName || newCourseName == courseNameOrig) return;
+
+    if (!_validateCourseName(newCourseName)) {
+      var msg = "The course name\n" + newCourseName + '\nis not valid.';
+      msg += '\n\nIt must have length between 1 and 200';
+      msg += ' and include only letters, digits, spaces, parentheses and commas.';
+      alert(msg);
+      return;
+    }
+    
+    settings.currentCourse.coursename = newCourseName;
+    
+    await _saveCourseInfo();
+  }  
+  
+  async function _addCourse(e) {  
+    var msg = 'Enter the name of the new course';
+    var courseName = prompt(msg);
+    if (!courseName) return;
+    
+    if (!_validateCourseName(courseName)) {
+      var msg = "The course name\n" + courseName + '\nis not valid.';
+      msg += '\n\nIt must have length between 1 and 200';
+      msg += ' and include only letters, digits, spaces, parentheses and commas.';
+      alert(msg);
+      return;
+    }
+    
+    var result = await _queryInsertCourse({coursename: courseName});
+    if (result.success) {
+      settings.currentCourse = {courseid: result.data.courseid};
+      await _updateCourseSelection();
+      
+    } else {
+      if (result.details.includes('duplicate')) {
+        alert('failed to add course\n A configuration for "' + courseName + '" already exists');
+      } else {
+        page.notice.setNotice(result.details);
+      }
+    }
+  }  
+  
+  async function _deleteCourse() {
+    if (!settings.currentCourse) return;
+
+    var msg = 'This course will be deleted:';
+    msg += '\n' + settings.currentCourse.coursename;
+    msg += '\n\nThis action cannot be undone.  Continue with deletion?';
+    
+    if (!confirm(msg)) return;
+
+    var result = await _queryDeleteCourse(settings.currentCourse);
+    if (!result.success) {
+      page.notice.setNotice(result.details);
+      return;
+    }
+
+    settings.currentCourse = null;
+    
+    await _updateCourseSelection();
+  }  
+  
   //---------------------------------------
 	// handlers
 	//----------------------------------------
@@ -118,6 +363,7 @@ const app = function () {
       "navConfiguration": async function() { await _showContents('navConfiguration'); },
       "navPreview":       async function() { await _showContents('navPreview'); },
       "navShare":         async function() { await _showContents('navShare'); },
+      "navRename":        async function() { await _renameCourse(); },
       
       "navEditOptions":   function() { _doOptions(); },
       
@@ -133,6 +379,28 @@ const app = function () {
     dispatchMap[dispatchTarget]();
   }
   
+  function _handleCourseSelection(e) {
+    _loadCourse(e.target[e.target.selectedIndex].courseInfo);
+  }
+  
+  async function _handleConfigChange(e) {    
+    await _saveCourseInfo();
+  }
+  
+  function _handleAudienceChange(e) {
+    console.log('_handleAudienceChange');
+    UtilityKTS.setClass(e.target.leftLabel, 'diminished', e.target.checked);
+    UtilityKTS.setClass(e.target.rightLabel, 'diminished', !e.target.checked);  
+  }
+  
+  function _handleDoubleSwitch(elem, clickedLabel) {
+    if (clickedLabel == 'left' && elem.checked) {
+      elem.click();
+    } else if (clickedLabel == 'right' && !elem.checked) {
+      elem.click();
+    }
+  }
+
   function _doOptions() {
     window.open(settings.optionsURL, '_self');
   }  
@@ -148,6 +416,32 @@ const app = function () {
   //---------------------------------------
 	// DB interface
 	//----------------------------------------
+  async function _queryCourseList() {
+    var dbResult = await SQLDBInterface.doGetQuery('welcomeV2/query', 'courselist');
+    
+    settings.courseInfo = null;
+    if (dbResult.success) {
+      settings.courseInfo = dbResult.data;
+    }
+    
+    return dbResult.success;
+  }
+  
+  async function _queryInsertCourse(courseInfo) {
+    return await SQLDBInterface.doPostQuery('welcomeV2/insert', 'course', courseInfo);
+  }
+  
+  async function _queryUpdateCourse(configurationInfo) {
+    return await SQLDBInterface.doPostQuery('welcomeV2/update', 'course', configurationInfo);
+  }
+
+  async function _queryDeleteCourse(courseInfo) {
+    return await SQLDBInterface.doPostQuery('welcomeV2/delete', 'course', courseInfo);
+  }
+  
+  async function _queryMailMessage(params) {
+    return await SQLDBInterface.doPostQuery('welcomeV2/query', 'mailmessage', params);
+  }
   
   //---------------------------------------
   // clipboard functions
@@ -167,7 +461,15 @@ const app = function () {
   //---------------------------------------
   // utility functions
   //----------------------------------------
-
+  function _validateCourseName(courseName) {
+    var valid = courseName.length < 200;
+    valid = valid && courseName.length > 0;
+    
+    valid = valid && (courseName.match(/[A-Za-z0-9&:\(\), ]+/) == courseName);
+    
+    return valid;
+  }  
+  
   //---------------------------------------
 	// return from wrapper function
 	//----------------------------------------
