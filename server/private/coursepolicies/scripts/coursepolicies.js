c9327f71a4d478f5b1b4afb8dcfde278fc2d4f33
SpunkyAardvark123 <ktsanter@gmail.com>
2022-09-09 07:04:53 -0500

    cte-department: added Sep dept mtg

--- server/private/coursepolicies/scripts/coursepolicies.js
+++ server/private/coursepolicies/scripts/coursepolicies.js
@@ -24,7 +24,6 @@ const app = function () {
 	// get things going
 	//----------------------------------------
 	async function init (sodium) {
-    console.log('test');
 		page.body = document.getElementsByTagName('body')[0]; 
     page.errorContainer = page.body.getElementsByClassName('error-container')[0];
     
